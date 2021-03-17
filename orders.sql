71652d7142bdcba67fdaf38d513efd12cb1c6af2
richak-github <51508449+richak-github@users.noreply.github.com>
2021-03-18 00:45:50 +0530

    Add files via upload

---
+++ orders.sql
@@ -0,0 +1,24 @@
+
+
+GRANT ALL PRIVILEGES ON *.* TO `root`@`%` WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+GRANT ALL PRIVILEGES ON *.* TO `order_user`@`%` WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+USE order_db;
+DROP TABLE IF EXISTS `orders`;
+
+
+CREATE TABLE `orders` (
+    `orderid` int NOT NULL AUTO_INCREMENT,
+	`name` text NOT NULL,
+    `customer_email` text,
+	`customer_address` text,
+    `order_price` double,
+    `quantity` int,
+    PRIMARY KEY (`orderid`)
+);
+
+
+
